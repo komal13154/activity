85f7e6233d5d42555f8f57bf9c0399b8001400c7
Komal <komaljha13154@gmail.com>
2026-09-12 09:35:25 +0530

    Time: 4 ms (87.53%), Space: 46.7 MB (47.37%) - LeetHub

---
+++ 0006-zigzag-conversion/0006-zigzag-conversion.java
@@ -0,0 +1,35 @@
+class Solution {
+    public String convert(String s, int numRows) {
+      if (numRows == 1 || numRows >= s.length()){
+        return s;
+      } 
+      StringBuilder[] rows = new StringBuilder[numRows];
+      for (int i = 0; i< numRows; i++){
+        rows[i] = new StringBuilder();
+      }
+      int currentRow = 0;
+      boolean goingDown = true;
+
+      for (char ch : s.toCharArray()){
+
+        rows[currentRow].append(ch);
+
+        if(currentRow == numRows - 1){
+            goingDown = false;
+        } else if (currentRow == 0){
+            goingDown = true;
+        }
+        if (goingDown){
+            currentRow++;
+        } else {
+            currentRow--;
+        }
+      }
+      StringBuilder result = new StringBuilder();
+
+      for (StringBuilder row : rows){
+        result.append(row);
+      }
+      return result.toString();
+    }
+}
