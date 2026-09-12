72be158ea153df23a01360d8df6d876c3a8dd89b
Komal <komaljha13154@gmail.com>
2026-09-12 14:03:39 +0530

    Time: 2754 ms (5%), Space: 0B (100%) - LeetHub

---
+++ 1581-customer-who-visited-but-did-not-make-any-transactions/1581-customer-who-visited-but-did-not-make-any-transactions.sql
@@ -0,0 +1,11 @@
+# Write your MySQL query statement below
+
+SELECT customer_id, COUNT(*) as count_no_trans
+FROM Visits as v
+LEFT JOIN 
+Transactions as t
+ON
+v.visit_id = t.visit_id
+WHERE
+t.transaction_id IS NULL
+GROUP BY customer_id
